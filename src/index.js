95b3682e276931d21b27acb677ba6bc8c2fdd0c7
Jonathan Williams <tamlin@mac.com>
2020-08-28 16:59:31 -0400

    add basic nav tree

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
-export default class extends Component {
+export default class Tree extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      treeData: [
+        { title: 'Home' },
+        { title: 'Admissions', children: [
+          { title: 'Undergraduate' },
+          { title: 'Graduate' }
+        ] },
+        { title: 'Academics', children: [
+          { title: 'Schools' },
+          { title: 'Programs' }
+        ] },
+        { title: 'Research' },
+        { title: 'Campus' },
+      ],
+    };
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={treeData => this.setState({ treeData })}
+        />
+      </div>
+    );
   }
 }
