97318a1f072c3d5fc997959dc73377926e99a137
Jonathan Williams <tamlin@mac.com>
2020-08-28 17:16:15 -0400

    rename primary component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SortableTree from 'react-sortable-tree';
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
-export default class Tree extends Component {
+export default class PageTree extends Component {
   constructor(props) {
     super(props);
 
